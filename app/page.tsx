6b8ba4de1a17029cb9bf5ba16771e4415445e81f
Yurii <olesich2007@gmail.com>
2026-08-10 20:05:35 +0300

    feature: added about-us section code and styles and imported it to the page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import AboutUs from '@/components/About/AboutUs';
+
 const HomePage = () => {
-  return <h1>Harmoniq</h1>;
+  return (
+    <>
+      <h1>Harmoniq</h1>
+      <AboutUs />
+    </>
+  );
 };
 
 export default HomePage;

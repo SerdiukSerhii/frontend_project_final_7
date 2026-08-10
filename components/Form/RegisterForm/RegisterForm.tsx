1a0396fccd6d295c3e0db174dddf06878ec872d0
Alina <alinavinnikhgfff@gmail.com>
2026-08-10 11:21:42 +0300

    add types for register form and create function handleSubmit

--- components/Form/RegisterForm/RegisterForm.tsx
+++ components/Form/RegisterForm/RegisterForm.tsx
@@ -1,17 +1,35 @@
 'use client';
 
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
 import Link from 'next/link';
 import { useId } from 'react';
+
+interface RegisterFormValues {
+  username: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+const initialValues: RegisterFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  repeatPassword: '',
+};
+
 export default function RegisterForm() {
   const fieldId = useId();
+
+  const handleSubmit = (values: RegisterFormValues, actions: FormikHelpers<RegisterFormValues>) => {
+    actions.resetForm();
+  };
   return (
     <>
       <h1>Register</h1>
       <p>Join our community of mindfulness and wellbeing!</p>
       <Formik
-        initialValues={{}}
-        onSubmit={() => {}}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
       >
         <Form>
           <label htmlFor={`${fieldId}-name`}>Enter your name</label>
@@ -19,12 +37,14 @@ export default function RegisterForm() {
             id={`${fieldId}-name`}
             type="text"
             name="username"
+            placeholder="Max"
           />
           <label htmlFor={`${fieldId}-email`}>Enter your name</label>
           <Field
             id={`${fieldId}-email`}
             type="email"
             name="email"
+            placeholder="email@gmail.com"
           />
           <label htmlFor={`${fieldId}-password`}>Enter your email address</label>
           <Field

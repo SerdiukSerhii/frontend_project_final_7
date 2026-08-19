5299282b4d8fb64022d3acfa3ac12e283370fee8
Alina <alinavinnikhgfff@gmail.com>
2026-08-19 17:33:08 +0300

    remove register btn for auth user

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import css from './Hero.module.css';
+import RegisterButton from './RegisterButton/RegisterButton';
 
 const Hero = () => {
   return (
@@ -24,12 +24,7 @@ const Hero = () => {
                 Go to Articles
               </a>
 
-              <Link
-                href="/register"
-                className={css.btnSecondary}
-              >
-                Register
-              </Link>
+              <RegisterButton className={css.btnSecondary} />
             </div>
           </div>
         </div>
